45bd8ab4b5b646c87d4ce4768c42eeeb01c9d7b3
Ben Schumacher <ben.schumacher@mattermost.com>
2022-10-20 12:20:54 -0700

    Fix linter errors

--- http-server/main.go
+++ http-server/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 
 	root "github.com/mattermost/mattermost-app-gcal"
 	function "github.com/mattermost/mattermost-app-gcal/function"
@@ -14,6 +15,13 @@ func main() {
 	function.AppPathPrefix = ""
 	function.Init()
 
-	fmt.Println("gcal App started, manifest at http://localhost:4444/manifest.json")
-	panic(http.ListenAndServe(":4444", nil))
+	listen := ":4444"
+	server := &http.Server{
+		Addr:              listen,
+		ReadHeaderTimeout: 3 * time.Second,
+		WriteTimeout:      5 * time.Second,
+	}
+
+	fmt.Printf("gcal App started, manifest at http://localhost%s/manifest.json\n", listen)
+	panic(server.ListenAndServe())
 }
